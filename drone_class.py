8a2d065a759a2246e8bef02d8b3db8b7d4c8de4b
Bram Bornhijm <brambornhijm@protonmail.com>
2020-11-21 17:38:29 +0100

    Added motor model

--- drone_class.py
+++ drone_class.py
@@ -1,40 +1,57 @@
 import numpy as np
 from pyquaternion import Quaternion
 
+
+"""
+Drone overview
+
+A      B
+ \\   //
+  \\ //
+    H
+  // \\
+ //   \\
+C       D
+"""
+
 class Drone:
 
-    def __init__(self, s0, s_dot0, dt, l=[0.2, 0.2, 0.2, 0.2], m=0.8):
+    def __init__(self, s0, s_dot0, dt, l=[0.2, 0.2, 0.2, 0.2], m=1.8, Is=[0.8, 0.8, 0.8]):
         self.s = np.asarray(s0) #[0, 0, 0, 0, 0, 0] x, y, z, pitch, roll, yaw
         self.s_dot = np.asarray(s_dot0) #ds/dt
+        self.s_ddot = np.asarray([0, 0, 0, 0, 0, 0])
         self.m = m
         self.dt = dt
 
-        self.motor_pos = np.asarray([[0, l[0], 0], [0, -l[2], 0], [l[1], 0, 0], [-l[3], 0, 0]])
+        self.I = np.asarray([[Is[0], 0, 0], [0, Is[1], 0], [0, 0, Is[2]]])
 
-        self.t = 0
-    
+        dr = [1, 1, -1, -1]
+
+        self.motor_pos = np.asarray([[0, l[0], 0], [0, -l[2], 0], [l[1], 0, 0], [-l[3], 0, 0]]) # motor positions are [A, D, B, C]
+        self.motors = [Motor(0.2, 150, 10, 0.2, dr[i], dt) for i in range(4)]
+
+        self.motor_commands = np.asarray([0, 0, 0, 0])
+        
     def update(self):
         T_M = self.get_transformation_matrix(*self.s[3:])[:3, :3]
         
+        F_tot, T_tot = np.zeros(3), np.zeros(3)
         #calculate motor forces
-        f_tot, t_tot = np.asarray([0,0,0]), np.asarray([0,0,0])
+        for motor, arm, command in zip(self.motors, self.motor_pos, self.motor_commands):
+            motor.set_omega_command(command)
+            F, T = motor.get_thrust_values()
 
-        for i, m_pos in enumerate(self.motor_pos):
-            #for now just use sines
-            motor_force = np.dot(T_M, np.asarray([0, 0, np.sin(self.t + i*0.25*np.pi) * 2]))
-            f_tot = np.add(f_tot, motor_force)
-            torque = np.cross(np.dot(T_M, m_pos), np.dot(T_M, motor_force))
-            t_tot = np.add(t_tot, torque)
+            F_global = np.dot(T_M, F)
+            T_global = np.add(np.dot(T_M, T), np.cross(np.dot(T_M, arm), np.dot(T_M, F)))
 
-        I = np.eye(3) * 0.8
+            F_tot = np.add(F_tot, F_global)
+            T_tot = np.add(T_tot, T_global)
 
-        rot_acc = np.dot(np.linalg.inv(I), t_tot)
-        acc = np.add(f_tot, np.asarray([0, 0, -9.81 * self.m])) / self.m
-
-        self.t += self.dt
+        ang_acc = np.dot(np.linalg.inv(self.I), T_tot)
+        acc = np.add(F_global / self.m, np.asarray([0, 0, -9.81])) 
 
         #for now no controllers or motors
-        a = np.append(acc, rot_acc)
+        a = np.append(acc, ang_acc)
         self.s_dot = np.add(self.s_dot, a * self.dt)
         self.s = np.add(self.s, self.s_dot * self.dt)
 
@@ -65,33 +82,57 @@ class Drone:
             drone = np.vstack((drone, np.dot(T, np.append(motor, 1.))[:-1]))
         
         return drone
+    
+    def IMU(self):
+        return np.append(self.s_dot, self.s_ddot) #vx, vy, vz, vpitch, vroll, vyaw, dvx, dvy, dvz, dvpitch, dvroll, dvyaw
+    
+    def gyro(self):
+        return self.s_dot[3:] #pitch, roll, yaw
+    
+    def altimeter(self):
+        return self.s[2] #altitude
+    
+    def eye_of_god(self):
+        return np.append(self.s, self.s_dot) #x, y, z, roll, pitch, yaw, vx, vy, vz, vpitch, vroll, vyaw 
+    
+    def set_motor_commands(self, motor_commands): 
+        self.motor_commands = np.asarray(motor_commands) #takes 4D array, using order [A, D, B, C]
 
 class Motor:
-
-    def __init__(self, thurst_coefficient, max_rot_vel, max_torque, propellor_intertia, direction, dt):
+    def __init__(self, thurst_coefficient, max_omega, max_torque, inertia, direction, dt):
         self.k = thurst_coefficient
-        self.max_rot_vel = max_rot_vel
-        self.propellor_intertia = propellor_intertia
-        self.max_torque = max_torque
-        self.dir = direction
+        self.max_rot_vel = max_omega
+        self.inertia = inertia
+        self.direction = direction
         self.dt = dt
+
+        self.max_angular_acceleration = max_torque / inertia
 
         self.omega = 0
         self.omega_ref = 0
+        self.t = 0
     
-    def set_omega_command(self, ref_omega):
-        self.ref_omega = ref_omega
-    
+    def set_omega_command(self, omega_ref):
+        if abs(self.omega_ref) > self.max_rot_vel:
+            self.omega_ref = self.max_rot_vel * np.sign(omega_ref) * self.direction
+        else:
+            self.omega_ref = omega_ref * self.direction
+        
     def update(self):
+        #model is assuming no required torque for constant velocity, model is also assuming max torque is constant over entire operating range of motor
         vel_error = self.omega_ref - self.omega
-        delta_omega_req = vel_error / self.dt
-        max_delta_omega = self.max_torque / self.propellor_intertia
+        ang_acc_req = vel_error / self.dt
 
-        if (delta_omega_req >= max_delta_omega):
+        if (np.abs(ang_acc_req) <= self.max_angular_acceleration):
             self.omega = self.omega_ref
+            self.t = -ang_acc_req * self.inertia
+        else:
+            self.omega += self.max_angular_acceleration * np.sign(vel_error) * self.dt
+            self.t = self.max_angular_acceleration * -np.sign(vel_error) * self.inertia
         
-
-    
-
-
-        
+    def get_thrust_values(self):
+        self.update()
+        #thrust is modelled as linear relationship with RPM
+        F = np.asarray([0, 0, self.k * self.omega])
+        T = np.asarray([0, 0, self.t])
+        return F, T

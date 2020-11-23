38e1320ffc70aae484bb221de9da4a6281f040a6
W. Meijer <houterm@gmail.com>
2020-11-24 00:50:35 +0100

    yaw controller added, restructure, failed funcanim experiment

--- controller.py
+++ controller.py
@@ -1,22 +1,23 @@
 import numpy as np
 from numpy.core.arrayprint import dtype_is_implied
+from numpy.core.numeric import roll
 
 class Controller:
     def __init__(self, drone):
         self.drone = drone
-        self.target = np.array([8, 5, 5]) # x, y, z
+        self.target = np.array([5, 5, 5]) # x, y, z
 
         self.Kp_z = 30
         self.Kd_z = 14
 
         self.Kp = 1
-        self.Kd = 0.5
+        self.Kd = 0
 
-        self.Kp_a = 1
-        self.Kd_a = 0.5
+        self.Kp_a = 10
+        self.Kd_a = 3
 
-        self.prev_error = np.array([0, 0, 0])
-        self.prev_angle_error = np.array([0, 0])
+        self.prev_local_pos_error = np.array([0, 0, 0])
+        self.prev_orientation_error = np.array([0, 0, 0])
         self.dt = 0.01
         self.time = 0
 
@@ -24,81 +25,131 @@ class Controller:
         state = self.drone.eye_of_god()
         # print("state", state)
 
+        print('x, y, z: ', state[0:3])
+
+        """OUTER LOOP PITCH & ROLL POSITION CONTROLLER"""
+        # x
+        # y
+            # by feeding into roll and pitch
+
         # rotation matrix to transform global pos error into local pos error
         rot_inv = np.linalg.inv(self.drone.get_transformation_matrix(state[3], state[4], state[5]))[:3, :3]
         #print(rot_inv)
 
-        pos_error = rot_inv@(self.target - state[0:3])
+        # world to body conversion of error
+        global_pos_error = self.target - state[0:3]
+        local_pos_error = rot_inv@global_pos_error
+        print("global_pos_error:", global_pos_error, " local pos error: ", local_pos_error)
 
-        d_error = (pos_error - self.prev_error)/self.dt
+        d_local_pos_error = (local_pos_error - self.prev_local_pos_error)/self.dt
 
-        print('Roll, Pitch, Yaw: ', state[3:6])
-        print('Pos: ', state[0:3])
+        angle_reference = self.Kp * \
+            local_pos_error[0:2] + self.Kd*d_local_pos_error[0:2]
+
+        roll_reference = angle_reference[0]
+        pitch_reference = angle_reference[1]
+
+        # rotate for me baby
+        yaw_reference = np.pi
+
+
+        """YAW + INNER LOOP PITCH & ROLL ORIENTATION CONTROLLER"""
+        # roll
+        # pitch 
+
+        # limit target orientation to 45deg
+        roll_reference = np.maximum(
+            np.minimum(roll_reference, np.pi/4),  -np.pi/4)
+        pitch_reference = np.maximum(
+            np.minimum(pitch_reference, np.pi/4),  -np.pi/4)
+
+        target_orientation = np.array(
+            [roll_reference, pitch_reference, yaw_reference])
+
+
+        orientation_error = target_orientation - state[3:6]
+
+        print('[Roll, Pitch, Yaw] state: ', state[3:6])
+        print('[Roll, Pitch, Yaw] target: ', target_orientation)
+        print("[Roll, Pitch, Yaw] error:", orientation_error)
+
+
+        d_orientation_error = (orientation_error -
+                               self.prev_orientation_error)/self.dt
+        d_orientation = self.Kp_a*orientation_error + self.Kd_a*d_orientation_error
+
+
+        pitch_cmd = d_orientation[0]
+        roll_cmd = d_orientation[1]
+        yaw_cmd = d_orientation[2]
+
+        """THRUST CONTROLLER"""
+        # thrust
 
         g = 9.81
-        # total_force = self.drone.m * g + self.Kp_z*pos_error[2] + self.Kd_z*d_error[2]
-        # TODO: verify. other drone code had everythig times mass 
+        # total_force = self.drone.m * g + self.Kp_z*local_pos_error[2] + self.Kd_z*d_local_pos_error[2]
+        # TODO: verify. other drone code had everythig times mass
         # https://github.com/AtsushiSakai/PythonRobotics/blob/40df009180e7541e6bc85e7f455d7d973a064aed/AerialNavigation/drone_3d_trajectory_following/drone_3d_trajectory_following.py#L83
-        total_force = self.drone.m * (g + self.Kp_z*pos_error[2] + self.Kd_z*d_error[2])
+        # total_force = self.drone.m * (g + self.Kp_z*local_pos_error[2] + self.Kd_z*d_local_pos_error[2])
 
-        #  Position to angle PD
-        angle_reference = self.Kp*pos_error[0:2] + self.Kd*d_error[0:2]
+        # trust controller
+        thrust_cmd = self.drone.m * \
+            (g + self.Kp_z*local_pos_error[2] + self.Kd_z*d_local_pos_error[2])
 
-        # # test with constant angle ref
-        # angle_reference = np.array([0, -np.pi/10])
-
-        # limit drone angle to 45deg
-        angle_reference = np.maximum(np.minimum(angle_reference, np.pi/4), -np.pi/4)
-        print("Angle ref:", angle_reference)
-
-        # Angle to forces PD
-        angle_error = angle_reference - state[3:5] #np.array([state[4], state[3]])
-        angle_d_error = (angle_error - self.prev_angle_error)/self.dt
-
-        print("angle_error:", angle_error)
-
-        d_angle = self.Kp_a*angle_error + self.Kd_a*angle_d_error
-
-        forces = np.ones((4))*total_force/4
-
-        # # pitch
-        # forces[2] -= d_angle[0]
-        # forces[3] += d_angle[0]
-
-        # # roll
-        # forces[0] -= d_angle[1]
-        # forces[1] += d_angle[1]
-
-        # pitch
-        forces[2] -= d_angle[0]
-        forces[3] += d_angle[0]
-
-        # roll
-        forces[0] -= d_angle[1]
-        forces[1] += d_angle[1]
+        # thrust_cmd = self.drone.m * g + self.Kp_z*local_pos_error[2] + self.Kd_z*d_local_pos_error[2]
 
 
-        forces = np.maximum(0, forces)
 
-        #forces = np.array([0, 0, 10, 10])
+        """MOTOR MIXING ALGORITHM"""
 
-        print("forces:", forces)
+        # motor_A = thrust_cmd + yaw_cmd
+        # motor_B = thrust_cmd - yaw_cmd
+        # motor_C = thrust_cmd - yaw_cmd
+        # motor_D = thrust_cmd + yaw_cmd
+
+        motor_A = thrust_cmd - roll_cmd - yaw_cmd
+        motor_B = thrust_cmd - pitch_cmd + yaw_cmd
+        motor_C = thrust_cmd + pitch_cmd + yaw_cmd
+        motor_D = thrust_cmd + roll_cmd - yaw_cmd
+        # motor cmd array as [ADBC]
+        motor_forces = np.array([motor_A, motor_D, motor_B, motor_C])
+        print("motor force pre:", motor_forces)
+
+
+        # TODO: drone does not know how to deal with negative forces, leading to zero motor input after reaching target.
+        # causing crash
+        motor_forces = np.maximum(1, motor_forces)
+
+        # std motor speed. add or substract difference
+
+        # if motor_forces.any() < 0:
+        #     motor_forces
+
+        # motor_force = np.maximum(
+        #     np.minimum(target_orientation, np.pi/8),  -np.pi/8)
+
+        print("motor force post:", motor_forces)
 
         # TODO: better way of sharing drone parameters
-        omega = np.sqrt(forces/self.drone.motors[0].k)
-        #print('Setpoint: ', omega)
+        # omega = np.sqrt(forces/self.drone.motors[0].k)
+
+
+        omega = np.sqrt(motor_forces/self.drone.motors[0].k)
+
+        print('Setpoint: ', omega)
         self.drone.set_motor_commands(omega)
 
-        self.prev_error = pos_error
-        self.prev_angle_error = angle_error
+        self.prev_local_pos_error = local_pos_error
+        # self.prev_angle_error = angle_error
+        self.prev_orientation_error = orientation_error
 
 
         # new target point
         self.time += self.dt
         # print("time:", self.time)
 
-        if self.time > 3:
-            self.set_target(np.array([5, 10, 5]))
+        # if self.time > 3:
+        #     self.set_target(np.array([5, 10, 5]))
 
 
     def set_target(self, pos):
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -25,12 +26,61 @@ def update(frame):
 fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
 
+# # wouter -------------------------
+# # doel is om live control waardes te plotten
+# # ax_debug = fig.add_subplot(projection="2d")
+
+# # t = np.linspace(0, 2*np.pi)
+# # x = np.sin(t)
+
+# fig2, ax2 = plt.subplots()
+# # l, = ax2.plot([0, 2*np.pi], [-1, 1])
+# # animate = lambda i: l.set_data(t[:i], x[:i])
+
+# x = 0
+# line, = ax2.plot(x, np.sin(x))
+# tijd = 0
+
+# xs = []
+# ys = []
+
+# def update2(i, xs, ys):
+
+#     tijd= 2
+
+#     # Add x and y to lists
+#     xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+#     ys.append(tijd)
+
+#     # Limit x and y lists to 20 items
+#     xs = xs[-20:]
+#     ys = ys[-20:]
+
+#     # Draw x and y lists
+#     ax2.clear()
+#     ax2.plot(xs, ys)
+
+#     # Format plot
+#     plt.xticks(rotation=45, ha='right')
+#     plt.subplots_adjust(bottom=0.30)
+#     plt.title('TMP102 Temperature over Time')
+#     plt.ylabel('Temperature (deg C)')
+#     # return line,
+
+
+# ani2 = animation.FuncAnimation(fig2, update2, fargs=(xs, ys), interval=dt**1000, blit=False)
+# plt.show()
+# # wouter --------------------------------
+
 ax.set_zlim3d([0.0, 20.0])
 ax.set_xlim3d([0.0, 20.0])
 ax.set_ylim3d([0.0, 20.0])
 ax.set_xlabel('$X$', fontsize=20)
 ax.set_ylabel('$Y$', fontsize=20)
 ax.set_zlabel('$Z$', fontsize=20)
+
+ax.view_init(azim=0, elev=90)
+
 
 x0,y0,z0 = 5,5,5
 # drone = Drone([x0, y0, z0, 0, 0, 0], [0, 0, 0, 0, 0, 0], dt, l=[0.2,0.2,0.2,0.2])
@@ -47,4 +97,6 @@ frame = ax.plot(p_frame[:, 0], p_frame[:, 1], p_frame[:, 2], 'k-')
 anim = motor_locations + frame
 
 ani = animation.FuncAnimation(fig, update, interval = dt**1000, blit=False)
+
+#TODO: plot error values
 plt.show()

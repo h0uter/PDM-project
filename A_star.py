399abe3b52d89d74750970e26b81ae2fcf80ff11
Bram B <61590608+bram-bor@users.noreply.github.com>
2020-12-20 15:14:50 +0100

    Update A_star.py
    fixed sign error

--- A_star.py
+++ A_star.py
@@ -40,7 +40,7 @@ class A_star:
                 update = True
 
                 if connected_node.label in self.priority_queue:
-                    update = (self.priority_queue[connected_node][path_cost] <= path_cost)
+                    update = (self.priority_queue[connected_node][path_cost] > path_cost)
 
                 if update:
                     self.priority_queue[connected_node.label] = {"object": connected_node,

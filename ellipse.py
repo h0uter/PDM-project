dc3ee39e6d19449bb00d3572177cd65497433f33
Bram Bornhijm <brambornhijm@protonmail.com>
2020-12-23 14:36:10 +0100

    fixed bug where ellipse would include points outside sample domain
    now when determining local radius in circle coordinates, it uses the minimum of the local radius and the minimal positive distance to hit a wall minus the normal margin

--- ellipse.py
+++ ellipse.py
@@ -3,9 +3,10 @@ import config as cfg
 
 class Ellipse:
 
-    def __init__(self, start, end):
+    def __init__(self, start, end, domain):
         self.start = start
         self.end = end
+        self.walls = Walls(domain=domain)
 
         self.a = np.linalg.norm(end - start) / 2 + cfg.dronehitbox_r
         self.r = None
@@ -66,8 +67,14 @@ class Ellipse:
         x = np.random.uniform(-self.a, self.a)
         phi = np.random.uniform(0, 2*np.pi)
 
-        local_radius = np.sqrt(1 - x**2 / self.a**2) * self.r
-        r = np.random.uniform(0, local_radius)
+        #find which is shorter, the local radius of the ellipse or the minimum distance to hit a 'domain wall'
+        local_radius_ellipse = np.sqrt(1 - x**2 / self.a**2) * self.r
+
+        global_origin = np.dot(self.transformation_matrix, np.asarray([x, 0., 0., 1]))[:-1]
+        unit_dir = np.dot(self.transformation_matrix[:-1, :-1], np.asarray([np.cos(phi), np.sin(phi), 0.]))
+        local_radius_domain = self.walls.get_minimum_distance(line_origin=global_origin, unit_dir=unit_dir)
+
+        r = np.random.uniform(0, min(local_radius_ellipse, local_radius_domain))
 
         #convert to cartesian
         y, z = np.cos(phi) * r, np.sin(phi) * r
@@ -78,3 +85,46 @@ class Ellipse:
     
     def ellipse_exists(self):
         return self.r is not None
+
+#make sure that created ellipse does not contain sample space that is not in the original space, ie is a subset
+class Walls:
+
+    def __init__(self, domain):
+        x_domain, y_domain, z_domain = domain
+        self.walls = []
+
+        self.walls.append(Wall(normal_vector=np.asarray([0, 0, 1]), p0=np.asarray([0, 0, z_domain[0] + cfg.dronehitbox_r]))) #floor
+        self.walls.append(Wall(normal_vector=np.asarray([0, 0, 1]), p0=np.asarray([0, 0, z_domain[1] - cfg.dronehitbox_r]))) #roof
+        self.walls.append(Wall(normal_vector=np.asarray([0, 1, 0]), p0=np.asarray([0, y_domain[0] + cfg.dronehitbox_r, 0]))) #wall #1
+        self.walls.append(Wall(normal_vector=np.asarray([0, 1, 0]), p0=np.asarray([0, y_domain[1] - cfg.dronehitbox_r, 0]))) #wall #2
+        self.walls.append(Wall(normal_vector=np.asarray([1, 0, 0]), p0=np.asarray([x_domain[0] + cfg.dronehitbox_r, 0, 0]))) #wall #3
+        self.walls.append(Wall(normal_vector=np.asarray([1, 0, 0]), p0=np.asarray([x_domain[1] - cfg.dronehitbox_r, 0, 0]))) #wall #4
+
+    def get_minimum_distance(self, line_origin, unit_dir):
+        min_dis = 1e6
+        for wall in self.walls:
+            d = wall.get_distance(line_origin, unit_dir)
+            if d is not None:
+                if d < min_dis and d >= 0.:
+                    min_dis = d
+        
+        return min_dis
+
+class Wall:
+
+    def __init__(self, normal_vector, p0):
+        self.normal_vector = normal_vector
+        self.p0 = p0
+    
+    def get_distance(self, line_origin, unit_dir):
+        numerator = np.dot(self.p0 - line_origin, self.normal_vector)
+        denominator = np.dot(unit_dir, self.normal_vector)
+
+        if denominator == 0:
+            return None
+        
+        elif numerator == 0:
+            return 0.0
+        
+        else:
+            return numerator / denominator
